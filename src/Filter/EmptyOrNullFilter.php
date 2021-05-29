b45be814e1acccfb305248d5f38c44ab6fc2736f
henk <info@metaclass.nl>
2021-05-29 13:54:51 +0200

    - Added comment to EmptyOrNullFilter

--- src/Filter/EmptyOrNullFilter.php
+++ src/Filter/EmptyOrNullFilter.php
@@ -1,13 +1,14 @@
 <?php
-
+/**
+ * Example of filter composition using QueryExpressionGeneratorInterface
+ * Copyright (c) MetaClass, Groningen, 2021
+ */
 
 namespace Metaclass\FilterBundle\Filter;
-
 
 use ApiPlatform\Core\Api\IdentifiersExtractorInterface;
 use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterTrait;
-use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\SearchFilterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\ExistsFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\QueryExpressionGeneratorInterface;
