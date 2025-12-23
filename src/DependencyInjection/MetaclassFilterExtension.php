5c784633e1dd4c0a260acff45fbcd77671bf9a80
henk <info@metaclass.nl>
2025-12-23 14:11:06 +0100

    - unneccesary use removed

--- src/DependencyInjection/MetaclassFilterExtension.php
+++ src/DependencyInjection/MetaclassFilterExtension.php
@@ -4,7 +4,6 @@ namespace Metaclass\FilterBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -19,7 +18,7 @@ class MetaclassFilterExtension extends Extension
      * {@inheritDoc}
      * @return void
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
 //        $configuration = new Configuration();
 //        $config = $this->processConfiguration($configuration, $configs);
